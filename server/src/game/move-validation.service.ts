8dad1e510000451551aa499722263996de6d07d0
Jeroen Offerijns <jeroen@offerijns.com>
2019-11-03 18:55:33 +0100

    refactor: move game-related services into separate package

--- server/src/game/move-validation.service.ts
+++ server/src/game/move-validation.service.ts
@@ -31,10 +31,6 @@ export class MoveValidationService {
             return 'You have to move diagonally'
         }
 
-        if (distanceRows > 2 || distanceCols > 2 && !isKing) {
-            return 'You cannot move this far'
-        }
-
         if (board[toRow][toCol] === player) {
             return 'You cannot move to a cell with a piece from the same player'
         }
@@ -44,22 +40,42 @@ export class MoveValidationService {
             const inbetweenCol = fromCol + ((toCol - fromCol) / 2)
 
             if (board[inbetweenRow][inbetweenCol].toLowerCase() === player) {
-            return 'You cannot jump over your own piece'
+                return 'You cannot jump over your own piece'
             }
 
             if (board[inbetweenRow][inbetweenCol] === ' ') {
-            return 'You cannot jump over your an empty cell'
+                return 'You cannot jump over your an empty cell'
             }
         }
 
-        if (!isKing && distanceRows === 1 && player === 'b' && fromRow > toRow) {
+        if (isKing) {
+            return this.isValidForAKing(board, fromRow, fromCol, toRow, toCol)
+        } else {
+            return this.isValidForANonKing(board, fromRow, fromCol, toRow, toCol)
+        }
+    }
+
+    private isValidForANonKing(board: Board, fromRow: number, fromCol: number, toRow: number, toCol: number): string {
+        const player = board[fromRow][fromCol].toLowerCase()
+        const distanceRows = Math.abs(toRow - fromRow)
+        const distanceCols = Math.abs(toCol - fromCol)
+
+        if (distanceRows > 2 || distanceCols > 2) {
+            return 'You cannot move this far'
+        }
+
+        if (distanceRows === 1 && player === 'b' && fromRow > toRow) {
             return 'You cannot move backwards with a non-king piece'
         }
 
-        if (!isKing && distanceRows === 1 && player === 'w' && fromRow < toRow) {
+        if (distanceRows === 1 && player === 'w' && fromRow < toRow) {
             return 'You cannot move backwards with a non-king piece'
         }
 
+        return 'OK'
+    }
+
+    private isValidForAKing(board: Board, fromRow: number, fromCol: number, toRow: number, toCol: number): string {
         return 'OK'
     }
 
