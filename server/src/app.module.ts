44472a82b249b50216e318f7086131b4cb484d37
Jeroen Offerijns <jeroen@offerijns.com>
2019-11-04 21:00:27 +0100

    fix: conversion to kings, clean up code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,14 +6,14 @@ import { StorageService } from './storage.service';
 import { BoardController } from './board/board.controller'
 import { BoardService } from './board/board.service'
 import { MoveValidationService } from './game/move-validation.service';
-import { MoveEvaluationService } from './game/move-evaluation.service';
+import { MoveGenerationService } from './game/move-generation.service';
 import { MinimaxService } from './ai/minimax.service';
 import { BoardEvaluationService } from './ai/board-evaluation.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BoardController],
-  providers: [StorageService, BoardService, BoardEvaluationService, MoveValidationService, MoveEvaluationService, MinimaxService],
+  providers: [StorageService, BoardService, BoardEvaluationService, MoveValidationService, MoveGenerationService, MinimaxService],
 })
 export class AppModule {
   
---
+++ server/src/game/move-generation.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@nestjs/common'
+
+import { Board, Player, Piece, Move, Turn } from '../board/board.service'
+import { MoveValidationService } from './move-validation.service'
+import { settings } from '../settings'
+
+/**
+ * These arrays denote the direction in which we are searching for moves.
+ * E.g. -1, -1 changes the row and column directions into negative values,
+ * thus searching in the top-left direction.
+ */
+const directions = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+
+@Injectable()
+export class MoveGenerationService {
+
+    constructor(private readonly moveValidationService: MoveValidationService) {}
+
+    getAllPossibleTurns(board: Board, player: Player): Turn[] {
+        let jumps: Turn[] = []
+        let moves: Turn[] = []
+
+        board.map((pieces: Piece[], row: number) => {
+            pieces.map((piece: Piece, col: number) => {
+                if (piece.toLowerCase() === player.toLowerCase()) {
+                    let hasJumps = false
+                    this.getJumpsFrom(board, row, col).map((turn: Turn) => {
+                        if (turn.length !== 0) {
+                            jumps.push(turn)
+                            hasJumps = true
+                        }
+                    })
+
+                    if (!hasJumps) {
+                        moves = [...moves, ...this.getMovesFrom(board, row, col)]
+                    }
+                }
+            })
+        })
+
+        // If you can jump, you must jump; only consider turns if no jumps are possible
+        if (jumps.length > 0) {
+            return jumps
+        } else {
+            return moves
+        }
+    }
+
+    getJumpsFrom(board: Board, row: number, col: number, previousTurn: Turn = []): Turn[] {
+        let turns = []
+
+        if (board[row][col] === 'B' || board[row][col] === 'W') {
+            // Search more than 2 steps away just for kings
+            for (let i = 2; i < settings.rowCount; i += 1) {
+                for (let j = 2; j < settings.colCount; j += 1) {
+                    // TODO
+                }
+            }
+        }
+
+        // Search 2 steps, so a single jump, away for both pawns and kings
+        directions.map(([rowdir, coldir]) => {
+            if (this.isValid(board, row, col, row + rowdir * 2, col + coldir * 2) === 'OK') {
+                const newTurn = [...previousTurn, { fromRow: row, fromCol: col, toRow: row + rowdir * 2, toCol: col + coldir * 2 }]
+                const withMultiJumps = this.getJumpsFrom(board, row + rowdir * 2, col + coldir * 2, newTurn)
+                turns = [...turns, ...withMultiJumps]
+            }
+        })
+
+        // Require multi-jumps if possible, otherwise just return the previous turn
+        if (turns.length > 0) {
+            return turns
+        } else {
+            return [previousTurn]
+        }
+    }
+
+    getMovesFrom(board: Board, row: number, col: number): Turn[] {
+        let turns = []
+
+        if (board[row][col] === 'B' || board[row][col] === 'W') {
+            // Search more than 1 step away just for kings
+            for (let r = 2; r < settings.rowCount; r += 1) {
+                for (let c = 2; c < settings.colCount; c += 1) {
+                    directions.map(([rowdir, coldir]) => {
+                        if (this.isValid(board, row, col, row + rowdir * r, col + coldir * c) === 'OK') {
+                            turns.push([{ fromRow: row, fromCol: col, toRow: row + rowdir * r, toCol: col + coldir * c }])
+                        }
+                    })            
+                }
+            }
+        }
+
+        // Search 1 step awway for both pawns and kings
+        directions.map(([rowdir, coldir]) => {
+            if (this.isValid(board, row, col, row + rowdir * 1, col + coldir * 1) === 'OK') {
+                turns.push([{ fromRow: row, fromCol: col, toRow: row + rowdir * 1, toCol: col + coldir * 1 }])
+            }
+        })
+
+        return turns
+    }
+
+    private isValid(board: Board, fromRow: number, fromCol: number, toRow: number, toCol: number) {
+        return this.moveValidationService.isValid(board, fromRow, fromCol, toRow, toCol)
+    }
+
+}
