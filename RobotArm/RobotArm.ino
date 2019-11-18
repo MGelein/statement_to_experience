3223b6049b680e57781aa9a8362f3d0ca4abab6d
Mees Gelein <meesgelein@gmail.com>
2019-11-18 12:29:08 +0100

    Most of the code should  be functional, now to test it on the actual model

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -7,6 +7,7 @@
  * T(SHOULDER ELBOW LINACT) //set trim
  * A(VALUE) //set acceleration
  */
+#include <Servo.h> 
 
 //The pins for each of the externals
 const int MAGNET_PIN = 12;
@@ -15,7 +16,7 @@ const int ELBOW_PIN = 6;
 const int LINACT_PIN = 9;
 
 //The variables used for the acceleration
-int acc = 5;
+int acc = 1;
 
 //The trim values for each of the servos, this is in microseconds
 int trimShoulder = 0;
@@ -26,6 +27,16 @@ int trimLinAct = 0;
 int targetShoulder = 1500;
 int targetElbow = 1500;
 int targetLinAct = 1500;
+//Change is the total change in this move, this is calculated when the move is parsed
+int changeShoulder = 0;
+int changeElbow = 0;
+int changeLinAct = 0;
+//Diff is the current amount of difference between position and target
+int diffShoulder = 0;
+int diffElbow = 0;
+int diffLinAct = 0;
+//Flag to see if we're done moving yet
+bool moveDone = false;
 
 //The positions of each of the servos and magnet
 int msShoulder = 1500;
@@ -35,7 +46,7 @@ int magnetState = LOW;
 
 //The servos used in this robot
 Servo shoulder;
-Servo eblow;
+Servo elbow;
 Servo linAct;
 
 //Variables used in the parsing of the commands
@@ -47,6 +58,7 @@ byte commandMode = NONE;
 byte numsRead = 0;
 
 int accumulator = 0;
+int elapsed = 0;
 unsigned long lastFrame = 0;
 unsigned long now = 0;
 unsigned long frameCount = 0;
@@ -63,26 +75,66 @@ void setup() {
   //Set up serial connection
   Serial.begin(115200);
   //Set up the pins in the correct pin mode
-  //pinMode(MAGNET_PIN, OUTPUT);
-  //pinMode(SHOULDER_PIN, OUTPUT);
-  //pinMode(ELBOW_PIN, OUTPUT);
-  pinMode(LINACT_PIN, OUTPUT);
+  pinMode(MAGNET_PIN, OUTPUT);
 }
 
 /**
  * Runs as often as possible
  */
 void loop() {
+  //Check if we're done moving
+  if(targetShoulder == msShoulder && targetElbow == msElbow && targetLinAct == msLinAct){
+    if(!moveDone){
+      moveDone = true;
+      Serial.println("OK");
+      //Write microseconds to each for the last time to finish this move
+      shoulder.writeMicroseconds(targetShoulder + trimShoulder);
+      elbow.writeMicroseconds(targetElbow + trimElbow);
+      linAct.writeMicroseconds(targetLinAct + trimLinAct);
+    }
+  }else{
+    //We're not done moving, so set this flag
+    moveDone = false;
 
+    //Calculate the current difference
+    diffShoulder = targetShoulder - msShoulder;
+    diffElbow = targetElbow - msElbow;
+    diffLinAct = targetLinAct - msLinAct;
+
+    //Now move towards the target for each of the servos
+    if(diffShoulder > acc) msShoulder += acc;
+    else if(diffShoulder < -acc) msShoulder -= acc;
+    else msShoulder += diffShoulder;
+    
+    if(diffElbow > acc) msElbow += acc;
+    else if(diffElbow < -acc) msElbow -= acc;
+    else msElbow += diffElbow;
+    
+    if(diffLinAct > acc) msLinAct += acc;
+    else if(diffLinAct < -acc) msLinAct -= acc;
+    else msLinAct += diffLinAct;
+
+    //Write the new position to the servos
+    shoulder.writeMicroseconds(targetShoulder + trimShoulder);
+    elbow.writeMicroseconds(targetElbow + trimElbow);
+    linAct.writeMicroseconds(targetLinAct + trimLinAct);
+  }
 
   frameCount ++;
   now = millis();
-  accumulator += now - lastFrame;
+  elapsed = now - lastFrame;
+  accumulator += elapsed;
   lastFrame = now;
   if(accumulator > 1000){
     accumulator -= 1000;
+    Serial.print("FPS ");
     Serial.print(String(frameCount));
-    Serial.println(" fps");
+    Serial.print("\t");
+    Serial.print(msShoulder);
+    Serial.print("\t");
+    Serial.print(msElbow);
+    Serial.print("\t");
+    Serial.println(msLinAct);
     frameCount = 0;
   }
   
@@ -90,6 +142,9 @@ void loop() {
   if(Serial.available() > 0){
     parseSerial();
   }
+
+  //Add a tiny bit of delay to allow for serial to buffer and stuff
+  delay(5);
 }
 
 /**
