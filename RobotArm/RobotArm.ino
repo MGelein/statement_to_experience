cd3930f367ffc3938a1f6d235c0af06ba5c2f2a1
Mees Gelein <meesgelein@gmail.com>
2019-11-20 17:34:19 +0100

    rewrote the robot arm commands, they are all different now

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -1,13 +1,15 @@
 /**
- * The code for the Robot Arm, this controls the servos and electro magnet
- * Messages are received over the Serial port.
- * 
- * We follow the following format:
- * M(SHOULDER ELBOW LINACT MAGNET) //move
- * T(SHOULDER ELBOW LINACT) //set trim
- * A(VALUE) //set acceleration
- */
-#include <Servo.h> 
+   The code for the Robot Arm, this controls the servos and electro magnet
+   Messages are received over the Serial port.
+
+   We follow the following format:
+   P(SHOULDER ELBOW)//set the position of each of the joints
+   L(LINACT)//set the linear actuator to a certain position
+   M(MAGNET)//set the magnet on or off
+   T(SHOULDER ELBOW LINACT) //set trim
+   A(VALUE) //set acceleration
+*/
+#include <Servo.h>
 
 const int MAX_SHOULDER = 2300;
 const int MIN_SHOULDER = 600;
@@ -57,11 +59,13 @@ Servo elbow;
 Servo linAct;
 
 //Variables used in the parsing of the commands
-const byte NONE = 0;
-const byte MOVE = 1;
-const byte ACC = 2;
-const byte TRIM = 3;
-byte commandMode = NONE;
+const byte CMD_NONE = 0;
+const byte CMD_MAGNET = 1;
+const byte CMD_ACC = 2;
+const byte CMD_TRIM = 3;
+const byte CMD_POS = 4;
+const byte CMD_LIN = 5;
+byte commandMode = CMD_NONE;
 byte numsRead = 0;
 bool firstMove = false;
 
@@ -72,8 +76,8 @@ unsigned long now = 0;
 unsigned long frameCount = 0;
 
 /**
- * Runs once when the power is turned on
- */
+   Runs once when the power is turned on
+*/
 void setup() {
   //Attach the servos to the correct pins, maybe detach as soon as there is nothing to do anymore
   shoulder.attach(SHOULDER_PIN);
@@ -85,7 +89,7 @@ void setup() {
   targetElbow = 1200;
   targetLinAct = 800;
   firstMove = true;
-  
+
   //Set up serial connection
   Serial.begin(115200);
   //Set up the pins in the correct pin mode
@@ -93,26 +97,26 @@ void setup() {
 }
 
 /**
- * Runs as often as possible
- */
+   Runs as often as possible
+*/
 void loop() {
   //Check if we're done moving
-  if(targetShoulder == msShoulder && targetElbow == msElbow && targetLinAct == msLinAct){
-    if(!moveDone){
+  if (targetShoulder == msShoulder && targetElbow == msElbow && targetLinAct == msLinAct) {
+    if (!moveDone) {
       moveDone = true;
       Serial.println("OK");
       //Write microseconds to each for the last time to finish this move
       shoulder.writeMicroseconds(targetShoulder + trimShoulder);
       elbow.writeMicroseconds(targetElbow + trimElbow);
       linAct.writeMicroseconds(targetLinAct + trimLinAct);
-      if(firstMove){
+      if (firstMove) {
         firstMove = false;
         targetShoulder = 1500;
         targetElbow = 1500;
         targetLinAct = 1500;
       }
     }
-  }else{
+  } else {
     //We're not done moving, so set this flag
     moveDone = false;
 
@@ -122,16 +126,16 @@ void loop() {
     diffLinAct = targetLinAct - msLinAct;
 
     //Now move towards the target for each of the servos
-    if(diffShoulder > acc) msShoulder += acc;
-    else if(diffShoulder < -acc) msShoulder -= acc;
+    if (diffShoulder > acc) msShoulder += acc;
+    else if (diffShoulder < -acc) msShoulder -= acc;
     else msShoulder += diffShoulder;
-    
-    if(diffElbow > 2 * acc) msElbow += 2 * acc;
-    else if(diffElbow < -2 * acc) msElbow -= 2 * acc;
+
+    if (diffElbow > 2 * acc) msElbow += 2 * acc;
+    else if (diffElbow < -2 * acc) msElbow -= 2 * acc;
     else msElbow += diffElbow;
-    
-    if(diffLinAct > 4 * acc) msLinAct += 4 * acc;
-    else if(diffLinAct < - 4 * acc) msLinAct -= 4 * acc;
+
+    if (diffLinAct > 4 * acc) msLinAct += 4 * acc;
+    else if (diffLinAct < - 4 * acc) msLinAct -= 4 * acc;
     else msLinAct += diffLinAct;
 
     //Write the new position to the servos
@@ -145,7 +149,7 @@ void loop() {
   elapsed = now - lastFrame;
   accumulator += elapsed;
   lastFrame = now;
-  if(accumulator > 1000){
+  if (accumulator > 1000) {
     accumulator -= 1000;
     Serial.print("FPS ");
     Serial.print(String(frameCount));
@@ -157,9 +161,9 @@ void loop() {
     Serial.println(msLinAct);
     frameCount = 0;
   }
-  
+
   //Check if any bytes can be read from the serial monitor
-  if(Serial.available() > 0){
+  if (Serial.available() > 0) {
     parseSerial();
   }
 
@@ -168,27 +172,30 @@ void loop() {
 }
 
 /**
- * Parse the serial input, try to find the command format
- */
-void parseSerial(){
+   Parse the serial input, try to find the command format
+*/
+void parseSerial() {
   //If no command mode has been set yet
-  if(commandMode == NONE){
+  if (commandMode == CMD_NONE) {
     char c = Serial.read();
-    if(c == 'M'){
-      commandMode = MOVE;
-      Serial.read();
-    }else if(c == 'T'){
-      commandMode = TRIM;
-      Serial.read();
-    }else if(c == 'A'){
-      commandMode = ACC;
-      Serial.read();
+    if (c == 'M' || c == 'm') {
+      commandMode = CMD_MAGNET;
+    } else if (c == 'T' || c == 't') {
+      commandMode = CMD_TRIM;
+    } else if (c == 'A' || c == 'a') {
+      commandMode = CMD_ACC;
+    } else if (c == 'P' || c == 'p') {
+      commandMode = CMD_POS;
+    } else if (c == 'L' || c == 'l') {
+      commandMode = CMD_LIN;
     }
-  }else{
+    //If we just found the start of a command, ignore the next char (whether it is a opening bracket or a space)
+    if (commandMode != CMD_NONE) Serial.read();
+  } else {
     //We are already in a command mode, first check if we need to close this command
-    if(Serial.peek() == ')'){
+    if (Serial.peek() == ')' || Serial.peek() == ';') {
       logReceivedCommand();
-      commandMode = NONE;
+      commandMode = CMD_NONE;
       numsRead = 0;
       Serial.read();
       return;
@@ -197,31 +204,31 @@ void parseSerial(){
     //If we make it to here, we can at least parse the num
     int num = Serial.parseInt();
     numsRead ++;
-    if(commandMode == MOVE){
-      if(numsRead == 1){
+    if (commandMode == CMD_POS) {
+      if (numsRead == 1) {
         targetShoulder = num;
         targetShoulder = constrain(targetShoulder, MIN_SHOULDER, MAX_SHOULDER);
-      }else if(numsRead == 2){
+      } else if (numsRead == 2) {
         targetElbow = num;
         targetElbow = constrain(targetElbow, MIN_ELBOW, MAX_ELBOW);
-      }else if(numsRead == 3){
-        targetLinAct = num;
-        targetLinAct = constrain(targetLinAct, MIN_LINACT, MAX_LINACT);
-      }else if(numsRead == 4){
-        //Write the magnet to the correct state immediately
-        digitalWrite(MAGNET_PIN, num > 0 ? HIGH : LOW);
-        magnetState = num;
       }
-    }else if(commandMode == TRIM){
-      if(numsRead == 1){
+    } else if (commandMode == CMD_LIN) {
+      targetLinAct = num;
+      targetLinAct = constrain(targetLinAct, MIN_LINACT, MAX_LINACT);
+    } else if (commandMode == CMD_MAGNET) {
+      //Write the magnet to the correct state immediately
+      digitalWrite(MAGNET_PIN, num > 0 ? HIGH : LOW);
+      magnetState = num;
+    } else if (commandMode == CMD_TRIM) {
+      if (numsRead == 1) {
         trimShoulder = num;
-      }else if(numsRead == 2){
+      } else if (numsRead == 2) {
         trimElbow = num;
-      }else if(numsRead == 3){
+      } else if (numsRead == 3) {
         trimLinAct = num;
       }
-    }else if(commandMode == ACC){
-      if(numsRead == 1){
+    } else if (commandMode == CMD_ACC) {
+      if (numsRead == 1) {
         acc = num;
       }
     }
@@ -229,23 +236,25 @@ void parseSerial(){
 }
 
 /**
- * This shows the received command in the Serial
- */
-void logReceivedCommand(){
-  if(commandMode == MOVE){
-    Serial.print("MOVE S(");
+   This shows the received command in the Serial
+*/
+void logReceivedCommand() {
+  if (commandMode == CMD_POS) {
+    Serial.print("POSITION S(");
     Serial.print(String(targetShoulder));
     Serial.print(") E(");
-    Serial.print(String(targetElbow));
-    Serial.print(") L(");
-    Serial.print(String(targetLinAct));
-    Serial.print(") M(");
+    Serial.println(String(targetElbow));
+  } else if (commandMode == CMD_LIN) {
+    Serial.print("LINACT L(");
+    Serial.println(String(targetLinAct));
+  } else if (commandMode == CMD_MAGNET) {
+    Serial.print("MAGNET M(");
     Serial.println(magnetState > 0 ? "HIGH)" : "LOW)");
-  }else if(commandMode == ACC){
-    Serial.print("ACC ");
+  } else if (commandMode == CMD_ACC) {
+    Serial.print("ACCELERATION ");
     Serial.println(String(acc));
     Serial.println("OK");
-  }else if(commandMode == TRIM){
+  } else if (commandMode == CMD_TRIM) {
     Serial.print("TRIM S(");
     Serial.print(String(trimShoulder));
     Serial.print(") E(");
